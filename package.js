5feca4c25a3a997fbc3aaa05455ad88b739b128a
Luca Mussi <ing.luca.mussi@gmail.com>
2014-09-06 20:24:49 +0200

    Bump to version 0.0.27

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     summary: "Accounts Templates styled for Semantic UI.",
-    version: "0.0.25",
+    version: "0.0.27",
     name: "splendido:accounts-templates-semantic-ui",
     git: "https://github.com/splendido/accounts-templates-semantic-ui.git",
 });
@@ -10,7 +10,7 @@ Package.on_use(function(api, where) {
         api.use([
             'service-configuration@1.0.0',
             'accounts-base@1.0.0',
-            'splendido:accounts-templates-core@0.0.25',
+            'splendido:accounts-templates-core@0.0.27',
             'templating@1.0.4',
             'less@1.0.5'
         ], 'client');
@@ -18,11 +18,11 @@ Package.on_use(function(api, where) {
             'service-configuration@1.0.0',
             'accounts-password@1.0.0',
             'accounts-base@1.0.0',
-            'splendido:accounts-templates-core@0.0.25',
+            'splendido:accounts-templates-core@0.0.27',
         ], 'server');
 
         api.imply([
-            'splendido:accounts-templates-core@0.0.25',
+            'splendido:accounts-templates-core@0.0.27',
             'service-configuration',
         ], ['client', 'server']);
     }
@@ -33,7 +33,7 @@ Package.on_use(function(api, where) {
             'accounts-templates-core',
             'templating',
             'less',
-        ], 'client');        
+        ], 'client');
         api.use([
             'service-configuration',
             'accounts-password',
@@ -62,14 +62,14 @@ Package.on_test(function(api) {
     if (api.versionsFrom) {
         api.use([
             'splendido:accounts-templates-semantic-ui',
-            'splendido:accounts-templates-core@0.0.25',
+            'splendido:accounts-templates-core@0.0.27',
         ]);
     }
     else{
         api.use([
             'accounts-templates-semantic-ui',
             'accounts-templates-core',
-        ]);        
+        ]);
     }
     api.use(['tinytest', 'test-helpers'], ['client', 'server']);
     api.add_files('tests/accounts-templates-semantic-ui_tests.js', ['client', 'server']);
