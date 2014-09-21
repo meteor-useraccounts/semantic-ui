7ea978f0aeaf6451ff7d574a8865bb8ba442cba1
Luca Mussi <ing.luca.mussi@gmail.com>
2014-09-21 08:08:31 +0200

    AT_field_ -> at-field-

--- package.js
+++ package.js
@@ -1,76 +1,73 @@
 Package.describe({
     summary: "Accounts Templates styled for Semantic UI.",
-    version: "0.0.30",
+    version: "0.9.2",
     name: "splendido:accounts-templates-semantic-ui",
     git: "https://github.com/splendido/accounts-templates-semantic-ui.git",
 });
 
 Package.on_use(function(api, where) {
-    if (api.versionsFrom) {
-        api.use([
-            'service-configuration@1.0.0',
-            'accounts-base@1.0.0',
-            'splendido:accounts-templates-core@0.0.28',
-            'templating@1.0.4',
-            'less@1.0.5'
-        ], 'client');
-        api.use([
-            'service-configuration@1.0.0',
-            'accounts-password@1.0.0',
-            'accounts-base@1.0.0',
-            'splendido:accounts-templates-core@0.0.28',
-        ], 'server');
+    api.versionsFrom("METEOR@0.9.2.2");
 
-        api.imply([
-            'splendido:accounts-templates-core@0.0.28',
-            'service-configuration',
-        ], ['client', 'server']);
-    }
-    else{
-        api.use([
-            'service-configuration',
-            'accounts-base',
-            'accounts-templates-core',
-            'templating',
-            'less',
-        ], 'client');
-        api.use([
-            'service-configuration',
-            'accounts-password',
-            'accounts-base',
-            'accounts-templates-core',
-        ], 'server');
+    api.use([
+        "less",
+        "templating",
+    ], "client");
 
-        api.imply([
-            'accounts-templates-core',
-            'service-configuration',
-        ], ['client', 'server']);
-    }
+    api.use([
+        "splendido:accounts-templates-core",
+    ], ["client", "server"]);
+
+    api.imply([
+        "splendido:accounts-templates-core@0.9.2",
+    ], ["client", "server"]);
+
     api.add_files([
+        'lib/at_error.html',
+        'lib/at_error.js',
         'lib/at_form.html',
         'lib/at_form.js',
         'lib/at_input.html',
         'lib/at_input.js',
+        'lib/at_oauth.html',
+        'lib/at_oauth.js',
+        'lib/at_pwd_form.html',
+        'lib/at_pwd_form.js',
+        'lib/at_pwd_form_btn.html',
+        'lib/at_pwd_form_btn.js',
+        'lib/at_pwd_link.html',
+        'lib/at_pwd_link.js',
+        'lib/at_result.html',
+        'lib/at_result.js',
+        'lib/at_sep.html',
+        'lib/at_sep.js',
+        'lib/at_signin_link.html',
+        'lib/at_signin_link.js',
+        'lib/at_signup_link.html',
+        'lib/at_signup_link.js',
         'lib/at_social.html',
         'lib/at_social.js',
+        'lib/at_terms_link.html',
+        'lib/at_terms_link.js',
+        'lib/at_title.html',
+        'lib/at_title.js',
         'lib/full_page_at_form.html',
         'lib/at_semantic-ui.less'
     ], ['client']);
 });
 
 Package.on_test(function(api) {
-    if (api.versionsFrom) {
-        api.use([
-            'splendido:accounts-templates-semantic-ui',
-            'splendido:accounts-templates-core@0.0.28',
-        ]);
-    }
-    else{
-        api.use([
-            'accounts-templates-semantic-ui',
-            'accounts-templates-core',
-        ]);
-    }
-    api.use(['tinytest', 'test-helpers'], ['client', 'server']);
-    api.add_files('tests/accounts-templates-semantic-ui_tests.js', ['client', 'server']);
+    api.use([
+        'accounts-templates-semantic-ui',
+        'splendido:accounts-templates-core@0.9.2',
+    ]);
+
+    api.use([
+        'accounts-password',
+        'tinytest',
+        'test-helpers'
+    ], ['client', 'server']);
+
+    api.add_files([
+        'tests/tests.js'
+    ], ['client', 'server']);
 });
